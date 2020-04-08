00189a79810ffac28c9dc3f90a0eb8d82817b133
bmark88 <bradley.mark@hotmail.com>
2020-04-08 19:34:00 +0000

    delete commented out code

--- public/scripts/view_manager.js
+++ public/scripts/view_manager.js
@@ -24,48 +24,5 @@ $(document).ready(() => {
         .prepend(newDiv);
     });
   });
-
-  // FOR PINS
-  // $.ajax({
-  //   type: 'GET',
-  //   url: '/pins/display'
-  // }).done(data => {
-  //   $('.pin-container').detach();
-  //   data.forEach(pin => {
-  //     const newDiv = $('<div></div>')
-  //       .addClass('pin-container');
-
-  //     const newThumbnail = $('<img>')
-  //       .attr('src', pin.thumbnail_url)
-  //       .addClass('pin-thumbnail');
-
-  //     const newTitle = $(`<h2>${pin.title}</h2>`)
-  //       .addClass('pin-title');
-
-  //     const newDescription = $(`<p>${pin.description}</p>`)
-  //       .addClass('pin-description');
-
-  //     const newTimestamp = $(`<p>Created at ${pin.created_at}</p>`)
-  //       .addClass('pin-date');
-
-  //     newDiv
-  //       .append(newThumbnail)
-  //       .append(newTitle)
-  //       .append(newDescription)
-  //       .append(newTimestamp)
-  //       .mouseenter(e => {
-  //         $(e.target)
-  //           .children()
-  //           .addClass('show');
-  //       })
-  //       .mouseleave(e => {
-  //         $(e.target)
-  //           .children()
-  //           .removeClass('show');
-  //       });
-  //     $('#pins-container')
-  //       .prepend(newDiv);
-  //   });
-  // });
 });
 

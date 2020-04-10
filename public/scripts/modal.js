88e21b6bdc84810b521ae28559668495d83e4fa2
bmark88 <bradley.mark@hotmail.com>
2020-04-10 08:08:52 +0000

    fix modal display and close bugs

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -25,31 +25,20 @@ function closeModal() {
 
 function outsideClick() {
   $(this).on('click', (e) => {
-    // const pin_id = $(e.target).parent().siblings('.comment-options').children('form')[0][0].value;
-    // console.log($(e.target).parent().children('.comment-options').children('form')[0][0].value)
-
-    // if($(e.target).parent().children('.comment-options').children('form')[0][0].value) {
-    //   console.log('fail')
-    //   const pin_id = $(e.target).parent().children('.comment-options').children('form')[0][0].value
-    // } else {
-
-    // console.log(e.target);
-
-    if ($('#simpleModal')[0] === $(e.target)[0]) {
-      $('#simpleModal').css('display', 'none');
-    }
-  
+    if($(e.target).attr('id') === 'simpleModal') {
+      $(e.target).css('display', 'none');
+    }  
   });
 }
 
 const displayModal = () => {
-  // console.log('hello from displayModal()')
-
   $(this).on('click', function(e) {
-    $(e.target).find('#simpleModal').css('display', 'block');
-  });
 
-  // $('.box').on('click', function () {
-  //   $(this).find('#simpleModal').css('display', 'block');
-  // })
+    if ($(e.target).attr('class') === 'box'){
+      $(e.target).find('#simpleModal').css('display', 'block');
+    }
+
+
+    // $(e.target).find('#simpleModal').css('display', 'block');
+  });
 };
